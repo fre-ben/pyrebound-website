f3c90727121cb514f7927de389bae07f76a6d6cc
frederik.schocke <frederik.schocke@aoe.com>
2026-03-31 16:59:26 +0200

    feat: add logo, socials, funding sections

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  integrations: [
+    icon({
+      include: {
+        pixel: ['linkedin', 'youtube', 'instagram', 'bluesky'],
+      },
+    }),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
