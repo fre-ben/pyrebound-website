74e923ace74b9de3d78c41801de9997379bea1ef
frederik.schocke <frederik.schocke@aoe.com>
2026-04-07 09:49:14 +0200

    ci: use basepath

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
+  base: '/pyrebound-website',
   integrations: [
     icon({
       include: {
